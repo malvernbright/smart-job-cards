e1773b28142e95bdf2c258dc2facf56204f888d6
LazyCoder <malvernbright.95@gmail.com>
2024-06-05 16:58:39 +0200

    Added Add Permissions button

--- resources/views/admin/permissions/index.blade.php
+++ resources/views/admin/permissions/index.blade.php
@@ -3,7 +3,9 @@
     <div class="py-12 w-full">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overfolw-hidden shadow-sm sm:rounded-lg p-2">
-
+                <div class="flex justify-end">
+                    <a class="px-4 py-2 bg-green-700 my-2 text-white hover:bg-green-500 rounded-md" href="#">Create</a>
+                </div>
 
                 <div class="relative overflow-x-auto">
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
